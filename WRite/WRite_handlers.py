474875acc91ea9efeb607a01e9ed5d905712b763
Ricky.Lim <rickylim19@gmail.com>
2014-06-26 19:42:15 +0700

    render write and show!

--- WRite/WRite_handlers.py
+++ WRite/WRite_handlers.py
@@ -5,7 +5,31 @@ from basehandler import basehandler
 from google.appengine.ext import db
 import time
 
+class Note(db.Model):
+    title = db.StringProperty(required = True)
+    note = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class WRite(basehandler.BaseHandler):
+    def render_write(self, title = '', note = '', error = ''):
+        notes = db.GqlQuery("SELECT * FROM Note "
+                            "ORDER BY created DESC ")
+        self.render("WRite.html", 
+                    title = title, note = note, error = error, notes = notes)
     def get(self):
-        self.render('WRite.html')
+        self.render_write()
+
+    def post(self):
+        title = self.request.get('title')
+        note = self.request.get('note')
+
+        if title and note:
+            NOTE = Note(title = title, note = note)
+            NOTE.put()
+            time.sleep(1)
+            self.redirect('/')
+        else:
+            error = "Please enter a title and a note"
+            self.render_write(title = title, note = note, error = error)
+
 
